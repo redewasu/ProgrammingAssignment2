1a25bb42a5c8d84f574fbafdf6f51e6a525e23dd
redewasu <redewasu@gmail.com>
2014-04-23 22:21:04 -0500

    Peer Assesment-R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes a square matrix and generates the inverse of it by cache or solving for the 
+##inverse if the matrix inverse has not been generated
+## 
 
-## Write a short comment describing this function
+## Function that caches a square matrix and generates it's inverse if getInverse is selected 
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(solve) m <<- solve
+    getInverse <- function() m
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getInverse = getInverse)
+  }
 
 
-## Write a short comment describing this function
+## Looks for the cache matrix and generates it's inverse if one has not been done, 
+## otherwise it just gets the cached matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached matrix")
+    return(m)
+  }
+  mat <- x$get()
+  m <-solve(mat, ...)
+  x$setSolve(m)
+  m
 }
+
+
